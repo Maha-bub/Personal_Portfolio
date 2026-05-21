6785911e84896bbcdf1fc966bf0a76cdb92250d9
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-21 19:16:53 +0600

    Create mobile menu and apply mobile menu toogle style

---
+++ script.js
@@ -0,0 +1,8 @@
+// MOBILE MENU TOGGLE
+    function toggleMobileMenu() {
+      const m = document.getElementById("mobileMenu");
+      m.style.display = m.style.display === "block" ? "none" : "block";
+    }
+
+
+    console.log('js file connected')
